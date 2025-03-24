6edfbf47cf6042156e2942c4dff80ac2af06bdf4
PanKibli <163906163+PanKibli0@users.noreply.github.com>
2025-03-24 07:51:45 +0100

    Update camera.gd

--- camera.gd
+++ camera.gd
@@ -2,15 +2,16 @@ extends Camera3D
 
 @onready var grid = %Grid
 var zoom = 1.0
-
-
-func _ready():
-	self.position = Vector3i(grid.gridSize.x/2,grid.gridSize.y*0.75, grid.gridSize.y)
-	print("CAMERA:")
-	print(self.position)
+var move_speed = 0.5
+var standard_postion := Vector3(6.0, 7.0, 10.0)
 	
 func _input(event):
-	var move_speed = 0.5
+	if event.is_action_pressed("camera_zoom_up") and zoom < 1.0:
+		transform.origin.y += move_speed
+		zoom += 0.1
+	elif event.is_action_pressed("camera_zoom_down") and zoom > 0:
+		transform.origin.y -= move_speed
+		zoom -= 0.1
 
 	if zoom != 1.0:
 		if event.is_action_pressed("ui_right"):
@@ -23,11 +24,6 @@ func _input(event):
 		elif event.is_action_pressed("ui_down"):
 			transform.origin.z += move_speed
 
-	if event.is_action_pressed("camera_zoom_up") and zoom < 1.0:
-		transform.origin.y += move_speed
-		zoom += 0.1
-	elif event.is_action_pressed("camera_zoom_down") and zoom > 0:
-		transform.origin.y -= move_speed
-		zoom -= 0.1
+	
 		
 	
