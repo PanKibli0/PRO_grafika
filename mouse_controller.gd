623fe4418ee7d4928aa9a4a438cf7792cde0f5e3
PanKibli <163906163+PanKibli0@users.noreply.github.com>
2025-04-26 18:39:05 +0200

    kill dorobic

--- mouse_controller.gd
+++ mouse_controller.gd
@@ -2,35 +2,40 @@ extends Node
 
 signal end_turn
 
+@onready var DIRECTION = %Direction
+
 @onready var GRID = %Grid
 @onready var CAMERA =  %Camera
 
-@onready var MARKER = $Marker
 
 var unit: Unit = null
 
 func _input(event):
 	if event is InputEventMouseMotion:  
-		var r_cell = _get_tile_at_mouse_position(event.position)
-		if r_cell == []: return
+		var r_cell = _get_cell_at_mouse_position(event.position)
+		if r_cell.is_empty(): 
+			DIRECTION.text = ""
+			return
 		var cell = r_cell[0]
 		var id = r_cell[1]
 		
 		if id == GRID.cell_type.MOVE:
 			GRID.select_cell(cell)
 		
-
 		if id == GRID.cell_type.ENEMY:
 			GRID.set_cell_item(GRID.selected_cell, 0)
 			GRID.selected_cell = Vector3i(-1,-1,-1)
 			unit = GRID.get_unit(cell)
 			unit.hp_debug(true)
+			update_attack_direction(cell, event.position)
 		elif unit != null:
 			unit.hp_debug(false)
+			DIRECTION.text = ""
+		
 		
 	if event.is_action_pressed("LMB") and !BATTLE.active_unit.is_moving:
-		var r_cell = _get_tile_at_mouse_position(event.position)
-		if r_cell == []: return
+		var r_cell = _get_cell_at_mouse_position(event.position)
+		if r_cell.is_empty(): return
 		var cell = r_cell[0]
 		var id = r_cell[1]
 		
@@ -38,9 +43,27 @@ func _input(event):
 			op_move(cell)
 			emit_signal("end_turn")
 		elif id == GRID.cell_type.ENEMY:
-			op_attack(cell)
-			emit_signal("end_turn")
+			if op_attack(cell, event.position): 
+				emit_signal("end_turn")
+	
+func _raycast(mouse_position: Vector2) -> Dictionary:
+	var space_state = CAMERA.get_world_3d().direct_space_state
+	var from = CAMERA.project_ray_origin(mouse_position)
+	var to = from + CAMERA.project_ray_normal(mouse_position) * 1000
+	return space_state.intersect_ray(PhysicsRayQueryParameters3D.create(from, to))
 		
+func _get_cell_at_mouse_position(mouse_position: Vector2) -> Array:
+	var result = _raycast(mouse_position)
+	if result.has("position"):
+		return [GRID.map_to_local(result.position), GRID.get_cell_item(result.position)]
+	return []
+	
+func get_mouse_world_position(mouse_position: Vector2) -> Vector3:
+	var result = _raycast(mouse_position)
+	if result.has("position"):
+		return result.position
+	
+	return Vector3(-1,-1,-1)
 			
 func op_move(cell): 
 	var world_pos = GRID.map_to_local(cell)
@@ -50,27 +73,51 @@ func op_move(cell):
 			
 	BATTLE.active_unit.move(world_pos) # PORUSZANIE JEDNSOTKA
 	
-
-func op_attack(cell: Vector3i):
+	
+	
+func op_attack(cell: Vector3i, mouse_position: Vector2) -> bool:
 	var attacked_unit = GRID.get_unit(cell)
-	if attacked_unit == null: return
+	if attacked_unit == null: 
+		return false
+
+	var enemy_pos = GRID.map_to_local(cell)
+	var mouse_world_pos = get_mouse_world_position(mouse_position)
+	if mouse_world_pos == Vector3(-1,-1,-1): return false
 	
-	var enemy_cell = cell
-	var enemy_pos = GRID.map_to_local(enemy_cell)
+	var click_dir = (mouse_world_pos - enemy_pos).normalized()
+
+
+	var delta = Vector3i(
+		1 if click_dir.x > 0.3 else (-1 if click_dir.x < -0.3 else 0),
+		0,
+		1 if click_dir.z > 0.3 else (-1 if click_dir.z < -0.3 else 0)
+	)
+
+
+	var desired_cell = cell + delta
 	var player_cell = GRID.local_to_map(BATTLE.active_unit.global_transform.origin)
-	
-	var space_state = CAMERA.get_world_3d().direct_space_state
-	var from = CAMERA.project_ray_origin(get_viewport().get_mouse_position())
-	var to = from + CAMERA.project_ray_normal(get_viewport().get_mouse_position()) * 1000
+	var standing_direction = cell - player_cell
 
-	
-	var query = PhysicsRayQueryParameters3D.create(from, to)
-	var result = space_state.intersect_ray(query)
 
-	var mouse_world_pos = Vector3.ZERO
-	if result.has("position"):
-		mouse_world_pos = result.position
+	if standing_direction == -delta:
+		attacked_unit.take_damage()
 
+	elif GRID.get_cell_item(desired_cell) in [GRID.cell_type.MOVE, GRID.cell_type.UNIT]:
+		if not GRID._is_cell_occupied(desired_cell) or GRID.get_unit(desired_cell) == BATTLE.active_unit:
+			op_move(desired_cell)
+			attacked_unit.take_damage()
+	else:
+		return false
+
+	return true
+
+
+
+
+func update_attack_direction(cell: Vector3i, mouse_position: Vector2):
+	var enemy_pos = GRID.map_to_local(cell)
+	var mouse_world_pos = get_mouse_world_position(mouse_position)
+	if mouse_world_pos == Vector3(-1,-1,-1): return 
 	var click_dir = (mouse_world_pos - enemy_pos).normalized()
 
 	var delta = Vector3i(
@@ -79,53 +126,22 @@ func op_attack(cell: Vector3i):
 		1 if click_dir.z > 0.3 else (-1 if click_dir.z < -0.3 else 0)
 	)
 
-	var desired_cell = enemy_cell + delta
-	var standing_direction = enemy_cell - player_cell
-
-	if standing_direction == delta:
-		perform_attack(attacked_unit, delta)
-	else:
-		op_move(desired_cell)
-		perform_attack(attacked_unit, delta)
-		
-
-func perform_attack(attacked_unit, direction: Vector3i):
-	attacked_unit.take_damage()
-	match direction:
-		Vector3i(1,0,0):
-			print("Atak od lewej")
-		Vector3i(-1,0,0):
-			print("Atak od prawej")
-		Vector3i(0,0,1):
-			print("Atak od przodu")
-		Vector3i(0,0,-1):
-			print("Atak od tyłu")
+	match delta:
+		Vector3i(-1, 0, 0):
+			DIRECTION.text = "Lewo"
+		Vector3i(1, 0, 0):
+			DIRECTION.text = "Prawo"
+		Vector3i(0, 0, -1):
+			DIRECTION.text = "Góra"
+		Vector3i(0, 0, 1):
+			DIRECTION.text = "Dół"
+		Vector3i(-1, 0, -1):
+			DIRECTION.text = "Lewo-Góra"
+		Vector3i(1, 0, -1):
+			DIRECTION.text = "Prawo-Góra"
+		Vector3i(-1, 0, 1):
+			DIRECTION.text = "Lewo-Dół"
+		Vector3i(1, 0, 1):
+			DIRECTION.text = "Prawo-Dół"
 		_:
-			print("Atak po skosie:", direction)
-
-
-
-func _get_tile_at_mouse_position(mouse_position) -> Array:
-	var space_state = CAMERA.get_world_3d().direct_space_state
-	var from = CAMERA.project_ray_origin(mouse_position)
-	var to = from + CAMERA.project_ray_normal(mouse_position) * 1000
-	
-	var query = PhysicsRayQueryParameters3D.create(from, to)
-	var result = space_state.intersect_ray(query)
-	
-
-	var hit_position: Vector3
-	if result.has("position"):
-		hit_position = result.position
-	else:
-		var ray_dir = (to - from).normalized()
-		var t = -from.y / ray_dir.y 
-		hit_position = from + ray_dir * t
-		hit_position.y = 0.55
-
-	MARKER.position = hit_position
-	
-	if result.has("position"):
-		return [GRID.map_to_local(result.position), GRID.get_cell_item(result.position)]
-		
-	return [GRID.selected_cell, GRID.get_cell_item(GRID.selected_cell)]
+			DIRECTION.text = ""
